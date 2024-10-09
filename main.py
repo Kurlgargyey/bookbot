bb70340226f2a417215348373354cb161f5b3a3e
Kurlgargyey <nahsirkivar@gmail.com>
2024-10-09 22:19:18 +0200

    added char counting feature

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ def main():
 		contents = f.read()
 		print(contents)
 		print(word_count(contents))
+		print(char_count(contents))
 
 def word_count(string) -> int:
 	return len(string.split())
+
+def char_count(string) -> dict:
+	tally = {}
+	for char in string.lower():
+		if not char.isalpha():
+			continue
+		if char in tally:
+			tally[char] += 1
+		else:
+			tally[char] = 1
+	return tally
 
 main()
