7b50431841723976ca5120efd528838ff5a58518
Kurlgargyey <nahsirkivar@gmail.com>
2024-10-09 22:40:47 +0200

    add basic reporting feature

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def main():
-	with open("books/frankenstein.txt") as f:
-		contents = f.read()
-		print(contents)
-		print(word_count(contents))
-		print(char_count(contents))
+	path = "books/frankenstein.txt"
+	with open(path) as book:
+		contents = book.read()
+		words = word_count(contents)
+		chars = char_count(contents)
+		print(report(path, words, chars))
 
 def word_count(string) -> int:
 	return len(string.split())
@@ -18,5 +19,17 @@ def char_count(string) -> dict:
 		else:
 			tally[char] = 1
 	return tally
+
+def report(path, word_count, char_count) -> str:
+	report = ""
+	report += f"report about book {path}\n\n"
+	report += f"the book contains {word_count} words! wow!\n"
+	for char, count in sorted(char_count.items(),
+                           key=lambda item: item[1],
+                           reverse=True):
+		report+= f"{char} occurs {count} times.\n"
+	report += "the report is done"
+	return report
+
 
 main()
