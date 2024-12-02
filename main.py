a01abcb3888fc5c364a8545feaa93de176519de1
Kurlgargyey <nahsirkivar@gmail.com>
2024-12-02 13:47:03 +0100

    refactored char_count to iterate over a filter in the first place instead of skipping over non-alpha chars. refactored report function to print the report directly.

--- main.py
+++ main.py
@@ -2,34 +2,24 @@ def main():
 	path = "books/frankenstein.txt"
 	with open(path) as book:
 		contents = book.read()
-		words = word_count(contents)
-		chars = char_count(contents)
-		print(report(path, words, chars))
+		report(path, word_count(contents), char_count(contents))
 
 def word_count(string) -> int:
 	return len(string.split())
 
 def char_count(string) -> dict:
 	tally = {}
-	for char in string.lower():
-		if not char.isalpha():
-			continue
-		if char in tally:
-			tally[char] += 1
-		else:
-			tally[char] = 1
+	for char in filter(lambda char: char.isalpha(), string.lower()):
+		tally[char] = tally.get(char, 0) + 1
 	return tally
 
 def report(path, word_count, char_count) -> str:
-	report = ""
-	report += f"report about book {path}\n\n"
-	report += f"the book contains {word_count} words! wow!\n"
+	print(f"report about book {path}\n")
+	print(f"the book contains {word_count} words! wow!")
 	for char, count in sorted(char_count.items(),
                            key=lambda item: item[1],
                            reverse=True):
-		report+= f"{char} occurs {count} times.\n"
-	report += "the report is done"
-	return report
-
+		print(f"{char} occurs {count} times.")
+	print("the report is done")
 
 main()
